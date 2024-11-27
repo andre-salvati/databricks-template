e7804c286349d97495c75f619ad85b87fe960e2d
Andre <andre.f.salvati@gmail.com>
2024-11-27 07:51:02 -0300

    medallion arch sample

--- src/template/extract_source1.py
+++ src/template/extract_source1.py
@@ -1,7 +1,6 @@
-from funcy import print_durations
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType
 
 from .baseTask import BaseTask
-from .commonSchemas import schema_template
 
 schema = "raw_source1"
 
@@ -10,33 +9,24 @@ class ExtractSource1(BaseTask):
     def __init__(self, config):
         super().__init__(config)
 
-    @print_durations
-    def transf1(self, df):
-        # TODO code your transformations here...
-
-        data = [("task1", "transf1")]
-        df_new = self.spark.createDataFrame(data, schema=schema_template)
-
-        return df.union(df_new)
-
     def run(self):
         print("Extracting data from Source1 ...")
 
         self.spark.sql(f"CREATE SCHEMA IF NOT EXISTS {schema}")
 
-        # prepare a simple input table
+        customer_schema = StructType(
+            [
+                StructField("id", IntegerType(), True),
+                StructField("name", StringType(), True),
+                StructField("country", StringType(), True),
+            ]
+        )
 
-        df = self.spark.createDataFrame([], schema=schema_template)
+        customer_data = [(10, "John Doe", "USA"), (20, "Jane Smith", "UK")]
 
-        df.write.mode("overwrite").saveAsTable(f"{schema}.table1")
-
-        # run transformations
-
-        df_in = self.spark.read.table(f"{schema}.table1")
-
-        df_out = self.transf1(df_in)
+        df = self.spark.createDataFrame(customer_data, schema=customer_schema)
 
         if self.config.get_value("debug"):
-            df_out.show()
+            df.show()
 
-        df_out.write.mode("overwrite").saveAsTable(f"{schema}.table2")
+        df.write.mode("overwrite").saveAsTable(f"{schema}.customer")
--- src/template/extract_source2.py
+++ src/template/extract_source2.py
@@ -1,40 +1,54 @@
+from pyspark.sql.types import (
+    FloatType,
+    IntegerType,
+    StringType,
+    StructField,
+    StructType,
+)
+
 from .baseTask import BaseTask
+
+schema = "raw_source2"
 
 
 class ExtractSource2(BaseTask):
     def __init__(self, config):
         super().__init__(config)
 
-    # @print_durations
-    # def transf2(self, df):
-
-    #     #TODO code your transformations here...
-
-    #     data = [("task2", "transf2")]
-    #     df_new = self.spark.createDataFrame(data, schema=schema_template)
-
-    #     return df.union(df_new)
-
-    # @print_durations
-    # def transf3(self, df):
-
-    #     #TODO code your transformations here...
-
-    #     data = [("task2", "transf3")]
-    #     df_new = self.spark.createDataFrame(data, schema=schema_template)
-
-    #     return df.union(df_new)
-
     def run(self):
-        print("source2...")
+        print("Extracting data from Source2 ...")
 
-    #    df_in = self.spark.read.table("table2")
+        self.spark.sql(f"CREATE SCHEMA IF NOT EXISTS {schema}")
 
-    #    df_transf2 = self.transf2(df_in)
+        order_schema = StructType(
+            [
+                StructField("id", IntegerType(), True),
+                StructField("id_customer", IntegerType(), True),
+                StructField("total", FloatType(), True),
+                StructField("date", StringType(), True),
+            ]
+        )
 
-    #    df_out = self.transf3(df_transf2)
+        order_item_schema = StructType(
+            [
+                StructField("id_order", IntegerType(), True),
+                StructField("seq", IntegerType(), True),
+                StructField("desc_item", StringType(), True),
+                StructField("qty", IntegerType(), True),
+                StructField("total_item", FloatType(), True),
+            ]
+        )
 
-    # if self.config.get_value("debug"):
-    #     df_out.show()
+        order_data = [(1, 10, 100.0, "2023-01-01"), (2, 20, 150.0, "2023-01-02")]
 
-    # df_out.write.mode("overwrite").saveAsTable("table3")
+        order_item_data = [(1, 1, "Item A", 2, 50.0), (1, 2, "Item B", 1, 50.0), (2, 1, "Item C", 3, 150.0)]
+
+        df_order = self.spark.createDataFrame(order_data, schema=order_schema)
+        df_order_item = self.spark.createDataFrame(order_item_data, schema=order_item_schema)
+
+        if self.config.get_value("debug"):
+            df_order.show()
+            df_order_item.show()
+
+        df_order.write.mode("overwrite").saveAsTable(f"{schema}.order")
+        df_order_item.write.mode("overwrite").saveAsTable(f"{schema}.order_item")
